d49e0118f093ca220961b7733961b5f5d6b3b05a
Shaun Cutts <shaun@cuttshome.net>
2016-10-05 13:41:07 -0400

    bind super call to "this"
    This caused a problem while using this addon in another addon.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ module.exports = {
   },
 
   included: function(app) {
-    this._super.included(app);
+    this._super.included.apply(this, arguments);
 
     app.import(app.bowerDirectory + '/perfect-scrollbar/js/perfect-scrollbar.js');
     app.import(app.bowerDirectory + '/perfect-scrollbar/css/perfect-scrollbar.css');
